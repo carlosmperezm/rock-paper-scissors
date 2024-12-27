9850700cd8caddfc4b89c61e551f965f93a38d8c
CarlosPerez <carlosmperez2901@gmail.com>
2024-12-27 13:09:18 -0800

    Add a function that validates who wins each round

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const choices = ['ROCK', 'PAPER', 'SCISSORS'];
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * choices.length);
@@ -14,4 +16,39 @@ function getHumanChoice() {
   }
 
   return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toUpperCase();
+  computerChoice = computerChoice.toUpperCase();
+  // Computer wins
+  if (humanChoice == 'ROCK' && computerChoice == 'PAPER') {
+    computerScore++;
+    return console.log('You lose! Paper beats Rock');
+  }
+  if (humanChoice == 'PAPER' && computerChoice == 'SCISSORS') {
+    computerScore++;
+    return console.log('You lose! SCISSORS beats Paper');
+  }
+  if (humanChoice == 'SCISSORS' && computerChoice == 'ROCK') {
+    computerScore++;
+    return console.log('You lose! Rock beats SCISSORS ');
+  }
+
+  // Human wins
+  if (humanChoice == 'PAPER' && computerChoice == 'ROCK') {
+    humanScore++;
+    return console.log('Computer lose! Paper beats Rock');
+  }
+  if (humanChoice == 'SCISSORS' && computerChoice == 'PAPER') {
+    humanScore++;
+    return console.log('Computer lose! SCISSORS beats PAPER');
+  }
+  if (humanChoice == 'ROCK' && computerChoice == 'SCISSORS') {
+    humanScore++;
+    return console.log('Computer lose! ROCK beats SCISSORS');
+  }
+  else {
+    return console.log('TIE')
+  }
 }
